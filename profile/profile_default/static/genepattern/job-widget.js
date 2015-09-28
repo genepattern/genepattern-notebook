8943edf35ec6d9c45d73d4a7c8513bfdd3a9dd61
tmtabor <tabor@broadinstitute.org>
2015-09-28 15:29:48 -0400

    Changed spacing to test new branch

--- profile/profile_default/static/genepattern/job-widget.js
+++ profile/profile_default/static/genepattern/job-widget.js
@@ -11,7 +11,12 @@
  * This software is supplied without any warranty or guaranteed support whatsoever. The Broad Institute is not
  * responsible for its use, misuse, or functionality.
  */
-define(["widgets/js/widget", "widgets/js/manager", "jqueryui", "/static/genepattern/gp.js", "/static/genepattern/navigation.js"], function (widget, manager) {
+define([
+    "widgets/js/widget",
+    "widgets/js/manager",
+    "jqueryui",
+    "/static/genepattern/gp.js",
+    "/static/genepattern/navigation.js"], function (widget, manager) {
     /**
      * Widget for viewing the job results of a launched job.
      *
