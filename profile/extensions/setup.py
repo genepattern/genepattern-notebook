daf1c646bea8bada063e08ec89cf1763c9c7c1d5
tmtabor <tabor@broadinstitute.org>
2016-02-04 16:00:34 -0500

    Updating to version 0.4.5

--- profile/extensions/setup.py
+++ profile/extensions/setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(name='genepattern-notebook',
     py_modules=['genepattern'],
-    version='0.4.4',
+    version='0.4.5',
     description='GenePattern Notebook extension for Jupyter',
     license='BSD',
     author='Thorin Tabor',
     author_email='tabor@broadinstitute.org',
     url='https://github.com/genepattern/genepattern-notebook',
-    download_url='https://github.com/genepattern/genepattern-notebook/archive/0.4.4.tar.gz',
+    download_url='https://github.com/genepattern/genepattern-notebook/archive/0.4.5.tar.gz',
     keywords=['genepattern', 'genomics', 'bioinformatics', 'ipython', 'jupyter'],
     classifiers=[
         'Development Status :: 4 - Beta',
