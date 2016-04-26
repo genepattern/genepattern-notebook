0215249b5ba69aff44d0b2c0712c99032fe8db01
tmtabor <tabor@broadinstitute.org>
2016-04-26 13:39:28 -0400

    Updating PIP distribution to include support files

--- extension/setup.py
+++ extension/setup.py
@@ -1,13 +1,38 @@
 from distutils.core import setup
-import subprocess
+from setuptools.command.install import install as _install
+from setuptools.command.develop import develop as _develop
+
+
+def _post_install():
+    import subprocess
+
+    # Enable the required nbextension for ipywidgets
+    subprocess.call(["jupyter", "nbextension", "enable", "--py", "widgetsnbextension"])
+
+    # Enable the GenePattern Notebook extension
+    subprocess.call(["jupyter", "nbextension", "install", "--py", "genepattern"])
+    subprocess.call(["jupyter", "nbextension", "enable", "--py", "genepattern"])
+    subprocess.call(["jupyter", "serverextension", "enable", "--py", "genepattern"])
+
+
+class GPInstall(_install):
+    def run(self):
+        _install.run(self)
+        self.execute(_post_install, [], msg="Running post install task")
+
+
+class GPDevelop(_develop):
+    def run(self):
+        _develop.run(self)
+        self.execute(_post_install, [], msg="Running post develop task")
 
 setup(name='genepattern-notebook',
-    py_modules=['genepattern'],
+    packages=['genepattern'],
     version='0.5.0',
     description='GenePattern Notebook extension for Jupyter',
     license='BSD',
     author='Thorin Tabor',
-    author_email='tabor@broadinstitute.org',
+    author_email='thorin@broadinstitute.org',
     url='https://github.com/genepattern/genepattern-notebook',
     download_url='https://github.com/genepattern/genepattern-notebook/archive/0.5.0.tar.gz',
     keywords=['genepattern', 'genomics', 'bioinformatics', 'ipython', 'jupyter'],
@@ -20,18 +45,12 @@ setup(name='genepattern-notebook',
         'Programming Language :: Python',
         'Framework :: IPython',
     ],
-    categories=[],
     install_requires=[
         'genepattern-python',
         'jupyter',
-        'ipywidgets'
+        'notebook>=4.2.0',
+        'ipywidgets>=5.0.0',
     ],
+    cmdclass={'install': GPInstall, 'develop': GPDevelop},
+    package_data={'genepattern': ['static/index.js', 'static/resources/*']},
 )
-
-# Enable the required nbextension for ipywidgets
-subprocess.call(["jupyter", "nbextension", "enable", "--py", "widgetsnbextension"])
-
-# Enable the GenePattern Notebook extension
-subprocess.call(["jupyter", "nbextension", "install", "--py", "genepattern"])
-subprocess.call(["jupyter", "nbextension", "enable", "--py", "genepattern"])
-subprocess.call(["jupyter", "serverextension", "enable", "--py", "genepattern"])
