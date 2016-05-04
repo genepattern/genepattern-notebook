a08f000f7fa128b5f09a55ebbe3adf5ad7eb98ac
tmtabor <tabor@broadinstitute.org>
2016-05-04 15:36:11 -0400

    Scaffolding and requirements for jupyter_wysiwyg nbextension

---
+++ wysiwyg/jupyter_wysiwyg/static/index.js
@@ -0,0 +1,29 @@
+// Add file path shim for Jupyter 3/4
+var WYSIWYG_PATH = location.origin + Jupyter.contents.base_url + "nbextensions/jupyter_wysiwyg/";
+
+define([
+    "base/js/namespace",
+    "jquery",
+    WYSIWYG_PATH + "ckeditor/ckeditor.js"], function(Jupyter) {
+
+    function load_ipython_extension() {
+        define([WYSIWYG_PATH + "ckeditor/adapters/jquery.js"], function() {
+            // Initiate the jupyter_wysiwyg extension
+            // If reloading a notebook, display with the full event model
+            // $([Jupyter.events]).on('kernel_ready.Kernel kernel_created.Session notebook_loaded.Notebook', GenePattern.notebook.init.notebook_init_wrapper);
+
+            // Otherwise, if not initialized after two seconds, manually init
+            setTimeout(function() {
+                //if (!GenePattern.notebook.init.done_init  && Jupyter.notebook.kernel) {
+                //    GenePattern.notebook.init.notebook_init_wrapper();
+                //}
+            }, 2000);
+
+            console.log("ALL IS WELL!");
+        });
+    }
+
+    return {
+        load_ipython_extension: load_ipython_extension
+    };
+});
