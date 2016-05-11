5f121f8c14fec205b411db96d0e914b45fae1e01
tmtabor <tabor@broadinstitute.org>
2016-05-11 11:50:21 -0400

    jupyter_wysiwyg nbextension now uses CKEditor's classic editing UI

--- wysiwyg/jupyter_wysiwyg/static/index.js
+++ wysiwyg/jupyter_wysiwyg/static/index.js
@@ -4,23 +4,30 @@ var WYSIWYG_PATH = location.origin + Jupyter.contents.base_url + "nbextensions/j
 define([
     "base/js/namespace",
     "jquery",
-    WYSIWYG_PATH + "ckeditor/ckeditor.js"], function(Jupyter) {
+    WYSIWYG_PATH + "ckeditor/ckeditor.js",
+    WYSIWYG_PATH + "ckeditor/adapters/jquery.js"], function(Jupyter) {
+
+    console.log("INSIDE FIRST: " + WYSIWYG_PATH + "ckeditor/adapters/jquery.js");
 
     function load_ipython_extension() {
-        define([WYSIWYG_PATH + "ckeditor/adapters/jquery.js"], function() {
-            // Initiate the jupyter_wysiwyg extension
-            // If reloading a notebook, display with the full event model
-            // $([Jupyter.events]).on('kernel_ready.Kernel kernel_created.Session notebook_loaded.Notebook', GenePattern.notebook.init.notebook_init_wrapper);
-
-            // Otherwise, if not initialized after two seconds, manually init
-            setTimeout(function() {
-                //if (!GenePattern.notebook.init.done_init  && Jupyter.notebook.kernel) {
-                //    GenePattern.notebook.init.notebook_init_wrapper();
-                //}
-            }, 2000);
-
+        //define([WYSIWYG_PATH + "ckeditor/adapters/jquery.js"], function() {
+        //    // Initiate the jupyter_wysiwyg extension
+        //    // If reloading a notebook, display with the full event model
+        //    // $([Jupyter.events]).on('kernel_ready.Kernel kernel_created.Session notebook_loaded.Notebook', GenePattern.notebook.init.notebook_init_wrapper);
+        //
+        //    // Otherwise, if not initialized after two seconds, manually init
+        //    //setTimeout(function() {
+        //    //    //if (!GenePattern.notebook.init.done_init  && Jupyter.notebook.kernel) {
+        //    //    //    GenePattern.notebook.init.notebook_init_wrapper();
+        //    //    //}
+        //    //}, 2000);
+        //
             console.log("ALL IS WELL!");
-        });
+        //
+        //    return {
+        //        load_jquery_adapter: function() {}
+        //    };
+        //});
     }
 
     return {
