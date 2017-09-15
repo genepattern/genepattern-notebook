b8f0eec2ef6146a4c48aebdeb49a32b9f1bd0cc6
tmtabor <tabor@broadinstitute.org>
2017-09-15 10:19:10 -0700

    Update version number

--- extension/setup.py
+++ extension/setup.py
@@ -41,7 +41,7 @@ class GPDevelop(_develop):
 
 setup(name='genepattern-notebook',
       packages=['genepattern'],
-      version='0.6.5.ALPHA',
+      version='0.6.5',
       description='GenePattern Notebook extension for Jupyter',
       license='BSD',
       author='Thorin Tabor',
