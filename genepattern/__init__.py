9b8ceec6859c7c055ef061f66837d743c167ae37
tmtabor <tabor@broadinstitute.org>
2018-03-05 09:48:20 -0800

    Updating version to 0.7.1

--- genepattern/__init__.py
+++ genepattern/__init__.py
@@ -10,7 +10,7 @@ from .jupyter_extensions import (GenePatternMagic, load_ipython_extension, load_
                                  _jupyter_server_extension_paths, _jupyter_nbextension_paths)
 
 __author__ = 'Thorin Tabor'
-__copyright__ = 'Copyright 2014-2017, Regents of the University of California & Broad Institute'
-__version__ = '0.7.0'
+__copyright__ = 'Copyright 2014-2018, Regents of the University of California & Broad Institute'
+__version__ = '0.7.1'
 __status__ = 'Beta'
 __license__ = 'BSD'
